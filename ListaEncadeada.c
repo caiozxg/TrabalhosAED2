b419e09b2b973ea78227337a9b603691d21ae074
caiozxg <105797281+caiozxg@users.noreply.github.com>
2024-06-09 13:32:25 -0300

    Update ListaEncadeada.c

--- ListaEncadeada.c
+++ ListaEncadeada.c
@@ -1,237 +1,188 @@
-# TrabalhosAED2
-
 #include <stdlib.h>
-
 #include <stdio.h> 
 #include <unistd.h>
 
-
-
-//utilizado para listas simplesmente encadeadas
-typedef struct no{
+// Estrutura para listas simplesmente encadeadas
+typedef struct no {
     int valor;
     struct no *proximo;
+} No;
 
-
-}No;
-
-///////////////////////////////////////////////////////////////////////////
-//utilizada para nao circular
 typedef struct {
-
     No *inicio;
     int tamanho;
-}Lista;
+} Lista;
 
 typedef struct {
-
     No *inicio;
     No *fim;
     int tamanho;
-}ListaCircular;
+} ListaCircular;
 
+
+
+///////////////////////////////////////////////////////////
 
 void iniciarlistaCircular(ListaCircular *listacircular);
 void iniciarlista(Lista *lista);
 No* busca1(Lista *lista, int ant);
-No* remover1(Lista *lista, int num);
+void remover1(Lista *lista, int num);
 void inserir1(Lista *lista, int num);
 void listaSimples(Lista *lista);
 void inserir2(ListaCircular *listacircular, int num);
-void listacircularmenu(ListaCircular *listacircular);
-void imprimir(Lista lista);
-void imprimircircular(ListaCircular listacircular);
-//////////////////////////////////////////////////////////////////////////
-void iniciarlistaCircular(ListaCircular *listacircular){
+void listaCircularmenu(ListaCircular *listacircular);
+void imprimir(Lista *lista);
+void imprimircircular(ListaCircular *listacircular);
+
+///////////////////////////////////////////////////////////////
+
+void iniciarlistaCircular(ListaCircular *listacircular) {
     listacircular->inicio = NULL;
     listacircular->fim = NULL;
     listacircular->tamanho = 0;
-    
-
-
 }
 
-
-
-void iniciarlista(Lista *lista){
+void iniciarlista(Lista *lista) {
     lista->inicio = NULL;
     lista->tamanho = 0;
-
-
 }
 
-/////////////////////////////////////////////////////////////////////////
-
-//busca lista simplesmente encadeada sem nó cabeça ordenada
-No* busca1(Lista *lista,int ant){
-    No *aux;
-    aux = NULL;
-    aux=lista->inicio;
-    while (aux->proximo != NULL && aux->valor != ant){
-        aux=aux->proximo;
+// Busca lista simplesmente encadeada sem nó cabeça ordenada
+No* busca1(Lista *lista, int ant) {
+    No *aux = lista->inicio;
+    while (aux != NULL && aux->valor != ant) {
+        aux = aux->proximo;
     }
-    if(aux == NULL){
-        printf("elemento nao encontrado");
-    
-    }else{
-        printf("elemento encontrado");
+    if (aux == NULL) {
+        printf("Elemento nao encontrado\n");
+    } else {
+        printf("Elemento encontrado\n");
     }
-        return aux;
+    return aux;
 }
-//remoção lista simplesmente encadeada sem nó cabeça ordenada
-No* remover1(Lista *lista,int num){
-   
-        No *aux,*remover = NULL;
-        aux = lista->inicio;
-        if (lista->inicio != NULL){
-        while (aux->proximo->valor != num && aux->proximo != NULL){
-            aux=aux->proximo;
 
+// Remoção lista simplesmente encadeada sem nó cabeça ordenada
+void remover1(Lista *lista, int num) {
+    No *aux = lista->inicio;
+    No *remover = NULL;
+    if (aux != NULL && aux->valor == num) {
+        remover = aux;
+        lista->inicio = aux->proximo;
+    } else {
+        while (aux != NULL && aux->proximo != NULL && aux->proximo->valor != num) {
+            aux = aux->proximo;
         }
-        if (aux->proximo){
-        remover=aux->proximo;
-        aux->proximo = remover->proximo;
+        if (aux != NULL && aux->proximo != NULL) {
+            remover = aux->proximo;
+            aux->proximo = remover->proximo;
+        }
+    }
+    if (remover != NULL) {
         free(remover);
-        }else{
-            printf("elemento nao encontrado");
-        }
-        }else{
-            printf("lista vazia!");
-        }
-
+        lista->tamanho--;
+        printf("Elemento removido\n");
+    } else {
+        printf("Elemento nao encontrado\n");
+    }
 }
 
-//////////////////////////////////////////////////////////////////////////////
-
-//inserção lista simplesmente encadeada sem nó cabeça ordenada
-void inserir1(Lista *lista,int num){
-    No *aux,*novo = malloc(sizeof(No));
-    //testar se o novo nó foi corretamente alocado
-    if (novo != NULL){
+// Inserção lista simplesmente encadeada sem nó cabeça ordenada
+void inserir1(Lista *lista, int num) {
+    No *aux, *novo = malloc(sizeof(No));
+    if (novo != NULL) {
         novo->valor = num;
-        //testamos se a lista esta vazia(será o primeiro numero da lista)
-        if (lista->inicio == NULL){
+        if (lista->inicio == NULL) {
             novo->proximo = NULL;
             lista->inicio = novo;
-        }
-        //caso o numero seja o menor de todos
-        else if(novo->valor < lista->inicio->valor){
+        } else if (novo->valor < lista->inicio->valor) {
             novo->proximo = lista->inicio;
             lista->inicio = novo;
-        }
-        //outros casos
-        else{
-
-            
+        } else {
             aux = lista->inicio;
-          
-            while (aux->proximo != NULL && novo->valor > aux->proximo->valor){
+            while (aux->proximo != NULL && novo->valor > aux->proximo->valor) {
                 aux = aux->proximo;
             }
-            novo->proximo=aux->proximo;
-            aux->proximo=novo;
+            novo->proximo = aux->proximo;
+            aux->proximo = novo;
         }
-         lista->tamanho++;
+        lista->tamanho++;
+    } else {
+        printf("Erro ao alocar a memoria!\n");
     }
-    
-    else{
-        printf("ERRO AO ALOCAR A MEMORIA!");
-    }
-
-
 }
 
-//1-lista simplesmente encadeada sem nó cabeça ordenada;
+// Lista simplesmente encadeada sem nó cabeça ordenada
 void listaSimples(Lista *lista) {
     printf("-----------------------------------------\n");
     printf("Operacoes em lista simplesmente encadeada\n");
-    int menu,num;
+    int menu, num;
 
-    // Loop para manter o menu sendo exibido até que o usuário decida sair
     do {
-        // Exibindo o menu
-    
         printf("\nMenu:\n");
         printf("1. Busca\n");
-        printf("2. Inserção\n");
-        printf("3. Remoção\n");
-        printf("4-imprimir");
+        printf("2. Insercao\n");
+        printf("3. Remocao\n");
+        printf("4. Imprimir\n");
         printf("0. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opcao: ");
         scanf("%d", &menu);
 
-        // Verificando a escolha do usuário usando switch case
         switch (menu) {
             case 1:
-                printf("qual numero voce deseja buscar?\n");
-                scanf("%d",&num);
-                busca1(&lista,num);
+                printf("Qual numero voce deseja buscar?\n");
+                scanf("%d", &num);
+                busca1(lista, num);
                 break;
             case 2:
-                printf("qual numero voce deseja inserir?\n");
-                scanf("%d",&num);
-                inserir1(&lista,num);
+                printf("Qual numero voce deseja inserir?\n");
+                scanf("%d", &num);
+                inserir1(lista, num);
                 break;
             case 3:
-                printf("qual numero voce deseja remover?\n");
-                scanf("%d",&num);
-                remover1(&lista,num);
+                printf("Qual numero voce deseja remover?\n");
+                scanf("%d", &num);
+                remover1(lista, num);
                 break;
             case 4:
                 imprimir(lista);
                 break;
             case 0:
                 printf("Saindo...\n");
-                exit(0);
                 break;
             default:
-                printf("Opção invalida! Por favor, escolha uma opção valida.\n");
+                printf("Opcao invalida! Por favor, escolha uma opcao valida.\n");
         }
     } while (menu != 0);
-
 }
-//2-Busca, inserção e remoção em lista circular simplesmente encadeada sem nó cabeça ordenada;
 
-//busca em circular
-
-
-//inserir em cicular
-void inserir2(ListaCircular *listacircular,int num){
-    No *aux,*novo = malloc(sizeof(No));
-    //testar se o novo nó foi corretamente alocado
-    if (novo != NULL){
+// Inserir em lista circular
+void inserir2(ListaCircular *listacircular, int num) {
+    No *aux, *novo = malloc(sizeof(No));
+    if (novo != NULL) {
         novo->valor = num;
-        //testamos se a lista esta vazia(será o primeiro numero da lista)
-        if (listacircular->inicio == NULL){
+        if (listacircular->inicio == NULL) {
             listacircular->inicio = novo;
-            listacircular->fim= novo;
+            listacircular->fim = novo;
             listacircular->fim->proximo=listacircular->inicio;
-
-        }
-        //caso o numero seja o menor de todos
-        else if(novo->valor < listacircular->inicio->valor){
+            //tambem insere no começo
+        } else if (novo->valor < listacircular->inicio->valor) {
             novo->proximo = listacircular->inicio;
             listacircular->inicio = novo;
-            listacircular->fim->proximo = novo;
-
-        }
-        //outros casos
-        else{
-
-            
+            listacircular->fim->proximo = listacircular->inicio;
+        } else {
             aux = listacircular->inicio;
-          
-            while (aux->proximo != NULL && novo->valor > aux->proximo->valor){
+            while (aux->proximo != listacircular->inicio && novo->valor > aux->proximo->valor) {
                 aux = aux->proximo;
             }
-            novo->proximo=aux->proximo;
-            aux->proximo=novo;
+            novo->proximo = aux->proximo;
+            aux->proximo = novo;
+            if (novo->proximo == listacircular->inicio) {
+                listacircular->fim = novo;
+            }
         }
-         listacircular->tamanho++;
-    }
-    
-    else{
-        printf("ERRO AO ALOCAR A MEMORIA!");
+        listacircular->tamanho++;
+    } else {
+        printf("Erro ao alocar a memoria!\n");
     }
 }
 
@@ -244,104 +195,139 @@ void inserir2(ListaCircular *listacircular,int num){
 
 
 
+No* remover2(ListaCircular *listacircular,int num){
+ No *aux, *remover = NULL;
 
-void listaCircularmenu(ListaCircular *listacircular){
-    printf("----------------------------------------------------------------\n");
-    printf("Operacoes em lista simplesmente encadeada,circular,sem no cabeca\n");
-    int menu,num;
+    if(listacircular->inicio){
+        if(listacircular->inicio == listacircular->fim && listacircular->inicio->valor == num){
+            remover = listacircular->inicio;
+            listacircular->inicio = NULL;
+            listacircular->fim = NULL;
+            listacircular->tamanho--;
+        }
+        else if(listacircular->inicio->valor == num){
+            remover = listacircular->inicio;
+            listacircular->inicio = remover->proximo;
+            listacircular->fim->proximo = listacircular->inicio;
+            listacircular->tamanho--;
+        }
+        else{
+            aux = listacircular->inicio;
+            while(aux->proximo != listacircular->inicio && aux->proximo->valor != num)
+                aux = aux->proximo;
+            if(aux->proximo->valor == num){
+                if(listacircular->fim == aux->proximo){
+                    remover = aux->proximo;
+                    aux->proximo = remover->proximo;
+                    listacircular->fim = aux;
+                }
+                else{
+                    remover = aux->proximo;
+                    aux->proximo = remover->proximo;
+                }
+                listacircular->tamanho--;
+            }
+        }
+    }
 
-    // Loop para manter o menu sendo exibido até que o usuário decida sair
+    return remover;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void listaCircularmenu(ListaCircular *listacircular) {
+    printf("------------------------------------------------------------------\n");
+    printf("Operacoes em lista simplesmente encadeada, circular, sem no cabeca\n");
+    int menu, num;
+
     do {
-        // Exibindo o menu
-    
         printf("\nMenu:\n");
         printf("1. Busca\n");
-        printf("2. Inserção\n");
-        printf("3. Remoção\n");
-        printf("4-imprimir");
+        printf("2. Insercao\n");
+        printf("3. Remocao\n");
+        printf("4. Imprimir\n");
         printf("0. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opcao: ");
         scanf("%d", &menu);
 
-        // Verificando a escolha do usuário usando switch case
         switch (menu) {
             case 1:
-                printf("qual numero voce deseja buscar?\n");
-                scanf("%d",&num);
-               // busca2(&listacircular,num);
+                printf("Qual numero voce deseja buscar?\n");
+                scanf("%d", &num);
+                // busca2(&listacircular, num); // Faltando implementação
                 break;
             case 2:
-                printf("qual numero voce deseja inserir?\n");
-                scanf("%d",&num);
-                inserir2(&listacircular,num);
+                printf("Qual numero voce deseja inserir?\n");
+                scanf("%d", &num);
+                inserir2(listacircular, num);
                 break;
             case 3:
-                printf("qual numero voce deseja remover?\n");
-                scanf("%d",&num);
-                //remover2(&listacircular,num);
+                printf("Qual numero voce deseja remover?\n");
+                scanf("%d", &num);
+                // remover2(&listacircular, num); // Faltando implementação
                 break;
             case 4:
                 imprimircircular(listacircular);
                 break;
             case 0:
                 printf("Saindo...\n");
-                exit(0);
                 break;
             default:
-                printf("Opção invalida! Por favor, escolha uma opção valida.\n");
+                printf("Opcao invalida! Por favor, escolha uma opcao valida.\n");
         }
     } while (menu != 0);
 }
 
-
-
-
-void imprimir(Lista lista){
-    //imprimiremos a lista(uma copia dela,por isso o .)
-    No *no=lista.inicio->proximo; 
-   
-    while (no !=NULL){
-        
-        
-        printf("->%d",no->valor);
-        no=no->proximo;
-         
+void imprimir(Lista *lista) {
+    No *no = lista->inicio; 
+    while (no != NULL) {
+        printf("->%d", no->valor);
+        no = no->proximo;
     }
-
-
-
+    printf("\n");
 }
 
-void imprimircircular(ListaCircular listacircular){
-    //imprimiremos a lista(uma copia dela,por isso o .)
-    No *teste;
-    teste=listacircular.inicio; 
-    while (teste != listacircular.inicio){
-        
-        
-        printf("->%d",teste->valor);
-        teste=teste->proximo;
-         
+void imprimircircular(ListaCircular *listacircular) {
+    if (listacircular->inicio == NULL) {
+        printf("Lista vazia\n");
+        return;
     }
-
-
-
+    No *teste = listacircular->inicio; 
+    do {
+        printf("->%d", teste->valor);
+        teste = teste->proximo;
+    } while (teste != listacircular->inicio);
+    printf("\n");
 }
 
+int main() {
+    int escolha;
+    Lista lista;
+    ListaCircular listacircular;
+    iniciarlista(&lista);
+    iniciarlistaCircular(&listacircular);
 
-
-
-
-
-//função principal
-int main(){
-
-     int escolha;
-     Lista lista;
-     ListaCircular listacircular;
-     iniciarlista(&lista);
-     iniciarlistaCircular(&listacircular);
-//menu para escolher qual opção sera usada
     do {
         printf("\nMenu:\n");
         printf("1. Busca, insercao e remocao em lista simplesmente encadeada sem no cabeca ordenada\n");
@@ -361,13 +347,13 @@ int main(){
                 listaCircularmenu(&listacircular);
                 break;
             case 3:
-            //    listaDuplamenteEncadeada();
+                // listaDuplamenteEncadeada(); // Faltando implementação
                 break;
             case 4:
-             //   fila();
+                // fila(); // Faltando implementação
                 break;
             case 5:
-              //  pilha();
+                // pilha(); // Faltando implementação
                 break;
             case 0:
                 printf("Saindo\n");
@@ -377,8 +363,5 @@ int main(){
         }
     } while (escolha != 0);
 
-
-
     return 0;
-
 }
