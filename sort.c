b71fc105b1623d3c4d5e481b549823413f7fdd08
Caio Angelis <105797281+caiozxg@users.noreply.github.com>
2024-09-26 14:33:30 -0400

    sort.c

---
+++ sort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void bubblesort(int *vector){
+    int temp;
+    for (int i=0;i<tam;i++){
+        if (vector[i]>vector[i+1]){
+            temp = vector[j];
+            vector[i]=vector[i+1];
+            vector[i+1]=temp;
+            }
+    }
+}
+
+void bubblesortmelhorado(int *vector){
+    int mudou=1;
+    // chega se mudou ou nao
+    while (mudou == 1){
+        int i = 0;
+        mudou = 0;
+        for (i<tam ; i++){
+            if (vector[i]>vector[i+1]){
+                temp = vector[j];
+                vector[i]=vector[i+1];
+                vector[i+1]=temp;
+                mudou = 1;
+            }
+        }
+
+
+
+    }
+
+}
+
+
+void insertionsort(int *vector){
+    //percorre aumentando 1 a cada vez
+    for (int i=1;i<tam;i++){
+        temp = vector[i];
+        int j = i-1;
+        //move os numeros maiores que os que estao guardado em temp para direita
+        while (j>=1 && temp < vector[j]){
+            vector[j+1] = vector[j];
+            j--;
+
+
+        }       
+        //adiciona o temp no lugar certo
+        vector[j+1] = temp;
+    }
+}
+
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
