3b6915301dbee6b6c2b818818d66c463fa420984
Caio Angelis <105797281+caiozxg@users.noreply.github.com>
2024-10-21 16:19:48 -0400

    Ordenacao.c

--- Ordenacao.c
+++ Ordenacao.c
@@ -1,1 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+
+typedef struct NoHeap{
+
+    char dado;
+    int frequencia;
+    struct NoHeap *Esquerda,*Direita;
+
+}NoHeap;
+
+
+typedef struct HeapMin{
+
+    unsigned tamanho;
+    unsigned capacidade;
+}HeapMin;
+
+
+
+
+
+int main(int argc,char *argv[]){
+    nome_arquivo = argv[1];
+    char *nome_arquivo = argv[2];
+    FILE *file =fopen(nome_arquivo,"rb");
+    int frequencias[256] = {0};
+    if (file == NULL){
+        printf("erro ao abrir o arquivo");
+        return 0;
+    }
+
+    char c;
+
+    while (fread(&c,sizeof(char),1,file)){
+
+        frequencias[(unsigned char)c]++;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
